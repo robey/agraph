225057dcfb9ae89e2ed9c84cc15458295e75550d
Robey Pointer <robeypointer@gmail.com>
2020-06-15 10:53:51 -0700

    oops

--- src/test/test_svg.ts
+++ src/test/test_svg.ts
@@ -5,7 +5,7 @@ import "source-map-support/register";
 
 const SVG_HEADER = `<?xml version="1.0" standalone="no"?>
 <!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">
-<svg width="100" height="100mm" viewBox="0 0 800 800" xmlns="http://www.w3.org/2000/svg" version="1.1">
+<svg width="100mm" height="100mm" viewBox="0 0 800 800" xmlns="http://www.w3.org/2000/svg" version="1.1">
   <desc>(none)</desc>
 `;
 const SVG_FOOTER = `</svg>\n`;
