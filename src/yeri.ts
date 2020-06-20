fcc9a6b985c99808329bf0a3a93c81e565ea298c
Robey Pointer <robeypointer@gmail.com>
2020-06-19 19:24:26 -0700

    add a top-level

---
+++ src/yeri.ts
@@ -0,0 +1,4 @@
+export { range } from "./arrays";
+export { buildSvgGraph, GraphInstant, SvgGraph, SvgGraphOptions } from "./svg_graph";
+export { CellData, Point, TimeSeries } from "./time_series";
+export { TimeSeriesList } from "./time_series_list";
