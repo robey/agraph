c5962893ca55c08e7749c64812d43ed4eeed6fba
Robey Pointer <robeypointer@gmail.com>
2020-08-16 14:21:27 -0700

    easier/better luma calc and correction -- makes no difference tho

--- src/rgb.ts
+++ src/rgb.ts
@@ -19,29 +19,14 @@ export class RGB {
     return (this.red << 16) + (this.green << 8) + this.blue;
   }
 
-  luma(): number {
-    // return 0.2126 * this.red + 0.7152 * this.green + 0.0722 * this.blue;
-    return 0.299 * this.red + 0.587 * this.green + 0.114 * this.blue;
+  // quick estimate of Y, using 0.375, 0.5, 0.125
+  approximateLuma(): number {
+    return (this.red * 3 + this.green * 4 + this.blue) >> 3;
   }
 
   correctLuma(desired: number): RGB {
-    const m = desired - this.luma();
-    if (Math.abs(m) >= 0.5) console.log("@@@ change", m)
-    // return this;
-    return new RGB(Math.round(this.red - m), Math.round(this.green - m), Math.round(this.blue - m));
-  }
-
-  // FIXME trash
-  toHCY(): number {
-    const max = Math.max(this.red, this.green, this.blue);
-    const min = Math.min(this.red, this.green, this.blue);
-    const chroma = max - min;
-    const hue = max == this.red ?
-      (this.green - this.blue) / chroma :
-      (max == this.green ? this.blue - this.red + 2 : this.red - this.green + 4);
-    const luma = 0.2126 * this.red + 0.7152 * this.green + 0.0722 * this.blue;
-
-    return (hue < 0 ? hue + 6 : hue) / 6;
+    const m = desired / this.approximateLuma();
+    return new RGB(Math.round(this.red * m), Math.round(this.green * m), Math.round(this.blue * m));
   }
 
   toHex(): string {
@@ -71,7 +56,7 @@ export class RGB {
 
   blendY(other: RGB, alpha: number): RGB {
     const nalpha = 1 - alpha;
-    const luma = this.luma() * alpha + other.luma() * nalpha;
+    const luma = this.approximateLuma() * alpha + other.approximateLuma() * nalpha;
     return new RGB(
       Math.ceil(this.red * alpha + other.red * nalpha),
       Math.ceil(this.green * alpha + other.green * nalpha),
@@ -94,6 +79,15 @@ export class RGB {
       Math.round(this.green * 1/3 + c1.green * 1/3 + c2.green * 1/3),
       Math.round(this.blue * 1/3 + c1.blue * 1/3 + c2.blue * 1/3),
     );
+  }
+
+  mix3y(c1: RGB, c2: RGB): RGB {
+    const luma = this.approximateLuma() * 1/3 + c1.approximateLuma() * 1/3 + c2.approximateLuma() * 1/3;
+    return new RGB(
+      Math.round(this.red * 1/3 + c1.red * 1/3 + c2.red * 1/3),
+      Math.round(this.green * 1/3 + c1.green * 1/3 + c2.green * 1/3),
+      Math.round(this.blue * 1/3 + c1.blue * 1/3 + c2.blue * 1/3),
+    ).correctLuma(luma);
   }
 
   distance(other: RGB): number {
@@ -127,8 +121,8 @@ export function quantize4to2(colors: RGB[]): RGB[] {
     triplet = [ merge[0][0], merge[0][1], merge[1][0] ];
   } else {
     // two pairs
-    const c1 = colors[merge[0][0]].blendx(colors[merge[0][1]], 0.5);
-    const c2 = colors[merge[1][0]].blendx(colors[merge[1][1]], 0.5);
+    const c1 = colors[merge[0][0]].blendY(colors[merge[0][1]], 0.5);
+    const c2 = colors[merge[1][0]].blendY(colors[merge[1][1]], 0.5);
     if (c1.number() != colors[merge[0][0]].number()) console.log(`merge ${rv[merge[0][0]].toHex()} + ${rv[merge[0][1]].toHex()} = ${c1.toHex()}`);
     if (c2.number() != colors[merge[1][0]].number()) console.log(`merge ${rv[merge[1][0]].toHex()} + ${rv[merge[1][1]].toHex()} = ${c2.toHex()}`);
     rv[merge[0][0]] = rv[merge[0][1]] = c1;
@@ -138,6 +132,6 @@ export function quantize4to2(colors: RGB[]): RGB[] {
 
   // funny math tricks: figure out the missing color
   const other = 6 - triplet[0] - triplet[1] - triplet[2];
-  rv[triplet[0]] = rv[triplet[1]] = rv[triplet[2]] = colors[triplet[0]].mix3x(colors[triplet[1]], colors[triplet[2]]);
+  rv[triplet[0]] = rv[triplet[1]] = rv[triplet[2]] = colors[triplet[0]].mix3(colors[triplet[1]], colors[triplet[2]]);
   return rv;
 }
